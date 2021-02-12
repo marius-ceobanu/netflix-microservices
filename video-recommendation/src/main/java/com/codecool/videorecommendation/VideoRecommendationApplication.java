8db50a166180e5d4f82b09ff8fa72561ea7371ee
Marius Ceobanu <ceobanu.marius@gmail.com>
2021-02-12 14:21:59 +0200

    Implement Eureka service discovery

--- video-recommendation/src/main/java/com/codecool/videorecommendation/VideoRecommendationApplication.java
+++ video-recommendation/src/main/java/com/codecool/videorecommendation/VideoRecommendationApplication.java
@@ -2,6 +2,7 @@ package com.codecool.videorecommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-//@EnableEurekaClient
+@EnableEurekaClient
 public class VideoRecommendationApplication {
 
 	public static void main(String[] args) {
